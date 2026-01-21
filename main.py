016ab9a1025ca35ad271d9749b9cbdbcc80fddf9
dlwnsh925 <dlwnsh925@gmail.com>
2026-01-21 10:32:40 +0900

    [Refactor] 260121 코드구조 재구성

---
+++ main.py
@@ -0,0 +1,67 @@
+# 반드시 SimulationApp이 가장 먼저 생성되어야 함
+from omni.isaac.kit import SimulationApp
+simulation_app = SimulationApp({"headless": False})
+
+import omni
+from isaacsim.sensors.physx import _range_sensor
+from core.env import EnvManager
+from core.safety import SafetyManager
+from core.move import HumanController
+
+def main():
+    usd_path = "/home/rokey/Desktop/DTHRC/DTHRC/assets/env_default.usd"
+    
+    # 1. 환경 관리자 초기화
+    env = EnvManager(usd_path)
+    env.setup_physics()
+    
+    stage = env.stage
+    timeline = omni.timeline.get_timeline_interface()
+    lidar_interface = _range_sensor.acquire_lidar_sensor_interface()
+
+    # 2. 각 모듈 초기화
+    safety = SafetyManager(stage, lidar_interface)
+    human_control = HumanController(stage, "/World/male")
+
+    # 3. 루프 변수
+    timeline.play()
+    frame_count = 0
+  
+
+    try:
+        while simulation_app.is_running():
+            simulation_app.update()
+            
+            if frame_count > 60:
+                current_time = timeline.get_current_time()
+                
+                # 거리 측정 및 로직 판단
+                dist = safety.get_human_distance()
+                
+                if dist is not None:
+                    if dist < 1.4:
+                        safety.change_led_color(1.0, 0.0, 0.0) # Red
+                        
+                    elif dist < 2.2:
+                        safety.change_led_color(1.0, 1.0, 0.0) # Yellow
+                        
+                    else:
+                        safety.change_led_color(0.0, 0.0, 1.0) # Blue
+                        
+                else:
+                    safety.change_led_color(0.0, 0.0, 1.0) # Blue (No detection)
+                    
+
+                # 사람 이동 업데이트
+                human_control.move_human(current_time)
+
+            frame_count += 1
+
+    except Exception as e:
+        print(f"Error occurred: {e}")
+    finally:
+        timeline.stop()
+        simulation_app.close()
+
+if __name__ == "__main__":
+    main()
